7369027b7f9ea2a6273013eef936c7a12725050d
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-10-11 21:09:24 +0330

    style: (new section)

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function HomePage() {
           <ImageCarousel />
         </section>
 
-        <section className="flex justify-between items-start mt-20 flex-col sm:flex-row gap-8">
+        <section className="flex justify-around items-start mt-20 flex-col sm:flex-row gap-8">
           <div>
             <h3 className="text-3xl font-bold text-primary py-5">چرا آشپزخانه کوروش</h3>
             <p className="max-w-96 tracking-wide leading-8 text-md sm:text-lg font-semibold">رستوران کوروش با افتخار ارائه‌دهنده‌ی غذاهای اصیل ایرانی با بالاترین کیفیت و طعمی ماندگار است. ما با استفاده از مواد اولیه تازه و مرغوب، غذاهایی تهیه می‌کنیم که هم خوش‌طعم‌اند و هم سالم. هدف ما، خلق تجربه‌ای دل‌انگیز از هر وعده غذایی برای شماست — چه در محیط گرم و صمیمی رستوران ما غذا میل کنید، چه سفارش خود را به‌صورت بیرون‌بر دریافت کنید.</p>
@@ -58,11 +58,25 @@ export default function HomePage() {
           </div>
         </section>
 
-        <section className="flex justify-center items-center gap-8 mt-20 transition-all">
-          <div className="w-60 h-34 bg-primary rounded-lg"></div>
-          <div className="w-60 h-34 bg-primary rounded-lg"></div>
-          <div className="w-60 h-34 bg-primary rounded-lg"></div>
-          <div className="w-60 h-34 bg-primary rounded-lg"></div>
+        <section className="flex justify-around items-start mt-20 flex-col sm:flex-row gap-8">
+          <div className="bg-primary pr-4 hover:pr-8 duration-200 rounded-xl">
+            <Image 
+              className="rounded-lg backdrop:opacity-25" 
+              src={"/caesar-salad.png"} 
+              height={500}
+              width={300} 
+              alt="caesar-salad-image"
+            />
+          </div>
+          <div>
+            <h3 className="text-3xl font-bold text-primary py-5">خدمات آشپزخانه</h3>
+            <ul className="max-w-96 flex flex-col gap-8">
+              <li><span className="text-primary font-bold">سرو غذاهای باکیفیت و تازه </span>استفاده از بهترین مواد اولیه و دستورهای اصیل برای تهیه‌ی غذاهایی خوش‌طعم و سالم.</li>
+              <li><span className="text-primary font-bold">سفارش بیرون‌بر و ارسال سریع </span>تحویل به‌موقع غذا با بسته‌بندی مناسب برای حفظ طعم و حرارت در مسیر.</li>
+              <li><span className="text-primary font-bold">محیطی دل‌نشین و خانوادگی </span>فضایی آرام و صمیمی برای صرف غذا با دوستان و خانواده.</li>
+              <li><span className="text-primary font-bold">پذیرش سفارشات ویژه </span>آماده‌سازی غذا برای مهمانی‌ها، مراسم و سفارشات گروهی با منوی متنوع و قیمت مناسب.</li>
+            </ul>
+          </div>
         </section>
 
         <section className="mt-20">
